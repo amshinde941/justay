4d1cfe52707f3fe99becb7bdf8a7cca87a6c541a
amshinde941 <amshinde941@gmail.com>
2021-05-20 22:34:40 +0530

    navbar

--- justay/src/components/homeComp.js
+++ justay/src/components/homeComp.js
@@ -1,61 +1,15 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import '../App.css';
-class Home extends Component {
+import React, {Component} from 'react';
+import Header from './headerComp';
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isNavOpen: false,
-        };
-        this.toggleNav = this.toggleNav.bind(this);
-
-    }
-    toggleNav() {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        });
+class Home extends Component{
+    render(){
+        return(
+            <div className="container">
+                <Header/>
+            </div>
+        )
     }
 
-
-    render() {
-        return (
-            <React.Fragment>
-                <Navbar dark className="navbar" expand="md" >
-                    <div className="container">
-                        <NavbarToggler onClick={this.toggleNav}/>
-                        <NavbarBrand className="mr-auto" href="/" ><img src="images/logo.png" height="30" width="41" alt="restorant amit"/>
-                        </NavbarBrand>
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                        <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/home">
-                                    <span className="fa fa-home fa-lg"></span>Home
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/aboutus">
-                                    <span className="fa fa-info fa-lg"></span>About Us
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/menu">
-                                    <span className="fa fa-list fa-lg"></span>Menu
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/contactus">
-                                    <span className="fa fa-address-card fa-lg"></span>Contact Us
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                        
-                        </Collapse>
-                    </div>
-                </Navbar>
-            </React.Fragment>
-        );
-    }
 }
+
 export default Home;
