7c95c42de971e8c09daab9f7239ec16a3a66010a
amshinde941 <amshinde941@gmail.com>
2021-05-21 15:19:15 +0530

    new

--- justay/src/components/homeComp.js
+++ justay/src/components/homeComp.js
@@ -15,6 +15,7 @@ class Home extends Component{
                             </div>
                         </div>
                 </Jumbotron>
+                
             </div>
         )
     }
